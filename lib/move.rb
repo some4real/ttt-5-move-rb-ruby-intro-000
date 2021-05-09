0cde88d6051b71205c20c7edd7b2f8daa0bd611b
Oluwaseyi Olufemi <oluwaseyi.olufemi.e@gmail.com>
2021-05-09 23:13:30 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,6 +1,14 @@
 # Tic Tac Toe Welcome page
 
 #input_to_index converts a user_input to an integer
-  user_input = "1"
-  converted_input = input_to_index(user_input)
-  
+
+def input_to_index(user_input)
+  new_user_input = user_input.to_i
+  new_user_input -= 1
+  return new_user_input
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+  return board
+end
